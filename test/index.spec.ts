f00cd59204d9edf9ea9fd0897ad5cea3bef4fe62
Ahmd Bilal <ahmadbilal.3491@gmail.com>
2024-09-05 12:22:34 +0500

    feat: init setup

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,4 +1,4 @@
-describe.skip('index', () => {
+describe('index', () => {
   describe('myPackage', () => {
     it('should return a string containing the message', () => {
       const message = 'Hello';
